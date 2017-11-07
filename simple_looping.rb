c6913aafc6d33eca278001b4df729407e55c0aae
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-07 22:06:28 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
     i = 0
-    puts phrase
+    print phrase
     i += 1
     break if i == number_of_times
   end
